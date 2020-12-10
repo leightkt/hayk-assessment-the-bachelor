99676018a866a4b4492768e73d48f356a7203623
Kat Leight <leightkt@gmail.com>
2020-12-09 19:23:22 -0700

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2055,9 +2055,19 @@ batch_hash = {
   ]
 }
 
+def get_contestants(data)
+  all_contestants = []
+  data.values.each do |contestants_array|
+    contestants_array.each do |contestant_hash|
+      all_contestants << contestant_hash
+    end
+  end
+  all_contestants
+end
+
+
 def get_first_name_of_season_winner(data, season)
 # code here
-# refactored
   data[season].find {|contestant|
     contestant["status"] == "Winner"
     return contestant["name"].split.first
@@ -2066,44 +2076,32 @@ end
  
 def get_contestant_name(data, occupation)
 # code here
-  data.values.each do |contestants|
-    contestants.each do |contestant|
-        if contestant["occupation"] == occupation
-          return contestant["name"]
-        end
-    end
-  end
+    get_contestants(data).find do |contestant|
+      contestant["occupation"] == occupation
+    end["name"]
 end
  
 def count_contestants_by_hometown(data, hometown)
  # code here
- count = 0
-  data.values.each do |contestants|
-    contestants.each do |contestant|
+    get_contestants(data).reduce(0) do |count, contestant|
       if contestant["hometown"] == hometown
-        count += 1
+        count + 1
+      else
+        count
       end
     end
-  end 
-  count
 end
  
 def get_occupation(data, hometown)
  # code here
-    data.values.each do |contestants|
-      contestants.each do |contestant|
-          if contestant["hometown"] == hometown
-            return contestant["occupation"]
-          end
-      end
-    end
+    get_contestants(data).find do |contestant|
+      contestant["hometown"] == hometown
+    end["occupation"]
 end
  
 def get_average_age_for_season(data, season)
 # code here
-# refactored
   (data[season].reduce(0) {|total_age, contestant|
     total_age + contestant["age"].to_f
   } / data[season].size).round
 end
-
